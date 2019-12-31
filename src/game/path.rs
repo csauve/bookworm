dc4e7a510a1481e7fecbf1944328fe8e8b3ca85c
csauve <sauvec@abebooks.com>
2019-12-31 12:33:19 -0800

    Complete impl and unit tests for path struct

--- src/game/path.rs
+++ src/game/path.rs
@@ -1,5 +1,4 @@
 use super::coord::Coord;
-use super::offset;
 use crate::api::ApiCoords;
 
 // A path connects a series of coordinates with direction. If nodes are only
@@ -7,7 +6,7 @@ use crate::api::ApiCoords;
 // an exact path between the start and end. If any consecutive pair of nodes has a non-linear
 // offset, the path between them is an undefined shortest manhattan path. All paths have at least
 // one node, in which case its length is 0 but it can still intersect.
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 pub struct Path {
     //todo: do nodes need to be public?
     pub nodes: Vec<Coord>,
@@ -15,41 +14,37 @@ pub struct Path {
 
 impl Path {
 
-    //todo: should we allow 0-length paths?
+    //todo: this is wrong... work on it
     pub fn new(arg_nodes: &[Coord]) -> Option<Path> {
         if arg_nodes.is_empty() {
-            return Option::None;
-        }
+            Option::None
+        } else if arg_nodes.len() < 3 {
+            Option::from(Path {nodes: Vec::from(arg_nodes)})
+        } else {
+            let mut nodes: Vec<Coord> = Vec::from(&arg_nodes[0..2]);
 
-        let mut nodes: Vec<Coord> = Vec::new();
-        let mut prev_offset = offset::ZERO;
-
-        //todo: this is wrong... work on it
-        for next in arg_nodes.iter() {
-            if let Some(prev) = nodes.last() {
-                let next_offset = *next - *prev;
-                if next_offset != prev_offset && *next != *prev {
-                    nodes.push(*next);
+            for &next in &arg_nodes[2..] {
+                let nodes_len = nodes.len();
+                let prev_prev = nodes[nodes_len - 2];
+                let prev = nodes[nodes_len - 1];
+                if prev.bounded_by(prev_prev, next) && (next - prev_prev).linear() {
+                    nodes[nodes_len - 1] = next;
+                } else {
+                    nodes.push(next);
                 }
-                prev_offset = next_offset;
-            } else {
-                //it's the first node, so add it regardless
-                nodes.push(*next);
             }
-        }
 
-        Option::from(Path {nodes})
+            Option::from(Path {nodes})
+        }
     }
 
-    //todo: convert to slice argument
-    pub fn init(coords: &Vec<ApiCoords>) -> Option<Path> {
-        let mapped_coords: Vec<_> = coords.iter().map(|c| Coord::init(*c)).collect();
+    pub fn init(coords: &[ApiCoords]) -> Option<Path> {
+        let mapped_coords: Vec<_> = coords.iter().map(|&c| Coord::init(c)).collect();
         Path::new(&mapped_coords)
     }
 
-    //todo: this isn't correct, work on it and add tests
     pub fn length(&self) -> u16 {
-        self.nodes.windows(2).fold(1, |total, pair| {
+        self.nodes.windows(2).fold(0, |total, pair| {
             total + (pair[1] - pair[0]).manhattan_dist()
         })
     }
@@ -77,11 +72,126 @@ mod tests {
 
     #[test]
     fn test_new() {
-        let path = Path::new(&[Coord::new(0, 0)]);
+        assert_eq!(Path::new(&[]), Option::None);
+
+        let mut path = Path::new(&[
+            Coord::new(1, 0),
+        ]).unwrap();
+
+        assert_eq!(path.nodes, &[
+            Coord::new(1, 0),
+        ]);
+
+        path = Path::new(&[
+            Coord::new(0, 0),
+            Coord::new(1, 0),
+        ]).unwrap();
+
+        assert_eq!(path.nodes, &[
+            Coord::new(0, 0),
+            Coord::new(1, 0),
+        ]);
+
+        path = Path::new(&[
+            Coord::new(0, 0),
+            Coord::new(1, 0),
+            Coord::new(2, 0),
+            Coord::new(2, 1),
+            Coord::new(2, 2),
+            Coord::new(3, 2),
+            Coord::new(3, 2),
+        ]).unwrap();
+
+        assert_eq!(path.nodes, &[
+            Coord::new(0, 0),
+            Coord::new(2, 0),
+            Coord::new(2, 2),
+            Coord::new(3, 2),
+        ])
     }
 
     #[test]
-    fn test_init() {
+    fn test_start_end() {
+        let mut path = Path::new(&[
+            Coord::new(0, 0),
+        ]).unwrap();
+
+        assert_eq!(path.start(), Coord::new(0, 0));
+        assert_eq!(path.end(), Coord::new(0, 0));
+
+        path = Path::new(&[
+            Coord::new(0, 0),
+            Coord::new(4, 0),
+            Coord::new(4, 10),
+            Coord::new(4, 10),
+        ]).unwrap();
+
+        assert_eq!(path.start(), Coord::new(0, 0));
+        assert_eq!(path.end(), Coord::new(4, 10));
     }
 
+    #[test]
+    fn test_length() {
+        assert_eq!(Path::new(&[
+            Coord::new(0, 0),
+        ]).unwrap().length(), 0);
+
+        assert_eq!(Path::new(&[
+            Coord::new(0, 0),
+            Coord::new(1, 0),
+        ]).unwrap().length(), 1);
+
+        assert_eq!(Path::new(&[
+            Coord::new(0, 0),
+            Coord::new(2, 0),
+        ]).unwrap().length(), 2);
+
+        assert_eq!(Path::new(&[
+            Coord::new(0, 0),
+            Coord::new(1, 1),
+        ]).unwrap().length(), 2);
+
+        assert_eq!(Path::new(&[
+            Coord::new(0, 0),
+            Coord::new(1, 0),
+            Coord::new(2, 0),
+            Coord::new(2, 1),
+            Coord::new(2, 2),
+            Coord::new(3, 2),
+            Coord::new(3, 2),
+        ]).unwrap().length(), 5);
+
+        assert_eq!(Path::new(&[
+            Coord::new(0, 0),
+            Coord::new(1, 0),
+            Coord::new(1, 1),
+            Coord::new(10, 10),
+            Coord::new(15, 12),
+        ]).unwrap().length(), 27);
+    }
+
+    #[test]
+    fn test_intersects() {
+        let mut path = Path::new(&[
+            Coord::new(0, 0),
+        ]).unwrap();
+
+        assert!(path.intersects(Coord::new(0, 0)));
+        assert!(!path.intersects(Coord::new(1, 0)));
+
+        path = Path::new(&[
+            Coord::new(0, 0),
+            Coord::new(10, 0),
+            Coord::new(10, 10),
+            Coord::new(20, 20),
+        ]).unwrap();
+
+        assert!(path.intersects(Coord::new(5, 0)));
+        assert!(!path.intersects(Coord::new(5, 1)));
+        assert!(path.intersects(Coord::new(10, 5)));
+        assert!(path.intersects(Coord::new(10, 10)));
+        assert!(path.intersects(Coord::new(15, 15)));
+        assert!(path.intersects(Coord::new(20, 20)));
+        assert!(!path.intersects(Coord::new(20, 21)));
+    }
 }
