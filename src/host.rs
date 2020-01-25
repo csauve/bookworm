bab41ca412c48e2cc6024af4a6cd91f209340bad
csauve <csauve@t3hz0r.com>
2020-01-25 12:29:52 -0800

    oh thats how

--- src/host.rs
+++ src/host.rs
@@ -2,8 +2,7 @@ use std::sync::Mutex;
 use futures::future;
 use log::*;
 use uuid::Uuid;
-use hyper::{Client, Request, Body, body};
-use serde_json::Result as JsonResult;
+use hyper::{Client, Request, Body, body, client::connect::HttpConnector};
 use crate::game::turn::Turn;
 use crate::game::snake::Snake;
 use crate::game::coord::UnitAbs;
@@ -15,32 +14,54 @@ struct LiveSnake {
     pub config: ApiSnakeConfig,
 }
 
-pub async fn run_game(_timeout_ms: u32, snakes_addrs: &[String], width: UnitAbs, height: UnitAbs) {
+async fn notify_start(client: &Client<HttpConnector>, addr: &str, game_state: ApiGameState) -> Result<ApiSnakeConfig, String> {
+    let req = Request::post(format!("{}/start", addr))
+        .body(Body::from(serde_json::to_string(&game_state).unwrap()))
+        .unwrap();
+    match client.request(req).await {
+        Err(e) => {
+            Err(format!("Snake @ {} failed to reply: {}", addr, e))
+        },
+        Ok(res) => {
+            let res_body = res.into_body();
+            match serde_json::from_slice::<ApiSnakeConfig>(&body::to_bytes(res_body).await.unwrap()) {
+                Ok(snake_config) => Ok(snake_config),
+                Err(e) => {
+                    Err(format!("Snake @ {} responded with invalid JSON: {}", addr, e))
+                }
+            }
+        }
+    }
+}
 
+pub async fn run_game(_timeout_ms: u32, snakes_addrs: &[String], width: UnitAbs, height: UnitAbs) {
     info!("Initializing {}x{} board", width, height);
     let mut turn = Turn::init(width, height, snakes_addrs.len()).unwrap();
     let mut turn_index: u32 = 0;
-    let game_id: ApiGameId = Uuid::new_v4().to_string();
-    let live_snakes: Mutex<Vec<LiveSnake>> = Mutex::new(Vec::new());
-    let mut client = Client::default(); //todo: set timeout
+    let game_id: ApiGameId = Uuid::new_v4().to_string();;
+    let client = Client::default(); //todo: set timeout
 
     info!("Notifying snakes of game start; id: {}", &game_id);
-    let responses = futures::future::join_all(
+    let start_responses = future::join_all(
         snakes_addrs.iter().enumerate().map(|(snake_index, addr)| {
-            async {
-                let game_state = build_api_game_state(&turn, snake_index, turn_index, &game_id);
-                let req = Request::post(format!("{}/start", addr))
-                    .body(Body::from(serde_json::to_string(&game_state).unwrap()))
-                    .unwrap();
-                let res = client.request(req)
-                    .await
-                    .expect(&format!("No response from snake @ {}", addr));
-                serde_json::from_slice::<ApiSnakeConfig>(&body::to_bytes(res.into_body()).await.unwrap());
-            }
+            let game_state = build_api_game_state(&turn, snake_index, turn_index, &game_id);
+            notify_start(&client, &addr, game_state)
         })
-    );
+    ).await;
+
+    let start_errs: Vec<String> = start_responses.iter().filter_map(|result| result.err()).collect();
+    if !start_errs.is_empty() {
+        for msg in start_errs.iter() {
+            error!("{}", msg);
+        }
+        error!("Some snake(s) failed to respond to the start call. Shutting down");
+        return;
+    }
+
+    let live_snakes: Mutex<Vec<LiveSnake>> = Mutex::new(start_responses.iter().map)
 
     while turn.snakes.len() > 1 {
+        info!("Requesting moves for turn {}", turn_index);
         let snake_moves = turn.snakes.iter().enumerate().map(|(i, snake)| {
             let game_state = build_api_game_state(&turn, i, turn_index, &game_id);
             // let req = Request::post(format!("{}/move", addr))
